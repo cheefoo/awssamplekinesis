aa57d9c7310bd280b0b8f269f49ad3413015aad8
Olajide <temitayo@amazon.com>
2016-09-09 11:05:12 -0500

    Initial Commit

---
+++ src/main/java/com/tayo/awssamplekinesis/AmazonKinesisApplicationRecordProcessorFactory.java
@@ -0,0 +1,17 @@
+package com.tayo.awssamplekinesis;
+
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessor;
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorFactory;
+
+/**
+ * Used to create new record processors.
+ */
+public class AmazonKinesisApplicationRecordProcessorFactory implements IRecordProcessorFactory {
+    /**
+     * {@inheritDoc}
+     */
+
+    public IRecordProcessor createProcessor() {
+        return new AmazonKinesisApplicationSampleRecordProcessor();
+    }
+}
